05f30ed5e91ccf239bedaf0b991a9c23a1f67ff8
Virgil_HD <120189340+virgilhg@users.noreply.github.com>
2025-06-18 20:52:41 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+// Note: Code incomplete. Challenge was time sensitive but with more time, I would have added get behind my info method to make it a getter property
+class Person {
+  constructor(name, age) {
+    this.name= name;
+    this.age = age;
+  }
+    Info(){
+      return `${this.name} age is ${this.age}`
+    } 
+}
